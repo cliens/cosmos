6bc1e17ab9998e017e4751d3ece367901d0a9ddd
cliens <cliens@sina.com>
2016-05-15 10:46:05 +0800

    add noop function

--- utils.js
+++ utils.js
@@ -10,3 +10,14 @@
 function reverseParams(iteratorFn) {
     return function(value, key) { iteratorFn(key, value); };
 }
+
+/**
+ * 空操作函数。在函数式编程的时候很有用
+ ```js
+ function foo(callback) {
+       var result = calculateResult();
+       (callback || noop)(result);
+     }
+ ```
+ */
+function noop() {}
