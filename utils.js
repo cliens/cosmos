73e3642131db306f7916185faca6bfcd47497dcb
zhanglc <cliens@sina.cn>
2016-11-15 10:23:42 +0800

    add date formatter

--- utils.js
+++ utils.js
@@ -71,3 +71,50 @@ function isBlob(obj) {
 function nodeName (element) {
     return lowercase(element.nodeName || (element[0] && element[0].nodeName));
 }
+
+
+/**
+ * 功用：将 Date 转化为指定格式的String
+ * 月(M)、日(d)、12小时(h)、24小时(H)、分(m)、秒(s)、周(E)、季度(q)可以用 1-2 个占位符，年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+ * <script>
+ *      formatDate(new Date(), "yyyy-MM-dd hh:mm:ss.S")==> 2006-07-02 08:09:04.423
+ *      formatDate(new Date(), "yyyy-MM-dd E HH:mm:ss") ==> 2009-03-10 二 20:09:04
+ *      formatDate(new Date(), "yyyy-MM-dd EE hh:mm:ss") ==> 2009-03-10 周二 08:09:04
+ *      formatDate(new Date(), "yyyy-MM-dd EEE hh:mm:ss") ==> 2009-03-10 星期二 08:09:04
+ *      formatDate(new Date(), "yyyy-M-d h:m:s.S") ==> 2006-7-2 8:9:4.18
+ * </script>
+ */
+window.formatDate = function (date, fmt) {
+    var _that = new Date(date);
+    var o = {
+        "M+": _that.getMonth() + 1, //月份
+        "d+": _that.getDate(), //日
+        "h+": _that.getHours() % 12 == 0 ? 12 : _that.getHours() % 12, //小时
+        "H+": _that.getHours(), //小时
+        "m+": _that.getMinutes(), //分
+        "s+": _that.getSeconds(), //秒
+        "q+": Math.floor((_that.getMonth() + 3) / 3), //季度
+        "S": _that.getMilliseconds() //毫秒
+    };
+    var week = {
+        "0": "/u65e5",
+        "1": "/u4e00",
+        "2": "/u4e8c",
+        "3": "/u4e09",
+        "4": "/u56db",
+        "5": "/u4e94",
+        "6": "/u516d"
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (_that.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    if (/(E+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "/u661f/u671f" : "/u5468") : "") + week[_that.getDay() + ""]);
+    }
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+}
